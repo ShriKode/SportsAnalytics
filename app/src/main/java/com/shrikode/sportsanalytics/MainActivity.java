800824caf9bc9306768de7b6e31a66aa884eb5fd
ShriKode <66577095+ShriKode@users.noreply.github.com>
2020-06-13 12:50:48 -0700

    Starting code for app.
    Login page code

--- app/src/main/java/com/shrikode/sportsanalytics/MainActivity.java
+++ app/src/main/java/com/shrikode/sportsanalytics/MainActivity.java
@@ -4,13 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    public void logIn(View view){
-        Log.i("Username", findViewById(R.id.usernameTextView).toString());
-        Log.i("Username", findViewById(R.id.usernameTextView).toString());
+        public void logIn(View view){
+
+            EditText username = findViewById(R.id.usernameTextView);
+            EditText password = findViewById(R.id.passwordTextView);
+            Log.i("Username", username.getText().toString());
+            Log.i("Password", password.toString());
+
+            Toast.makeText(MainActivity.this, "Hi there, " + username.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 
 
